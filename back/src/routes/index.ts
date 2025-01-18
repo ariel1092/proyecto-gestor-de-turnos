822dea5e087a475a3220b9f877ee32f40f1418b9
ariel1092 <fede_10_92@hotmail.com>
2025-01-18 14:59:32 -0300

    se agrega la ruta /get

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -3,6 +3,9 @@ import userRoute from "./userRoute"
 import appoinmentRouter from './appoinmentsRoutes'
 const router: Router = Router()
 
+router.get("/", (req, res) => {
+    res.status(200).send("Bienvenido al backend del proyecto Gestor de Turnos.");
+});
 
 router.use("/users", userRoute)
 router.use("/appoinments", appoinmentRouter)
